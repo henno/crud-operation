23bb3ef203467b241dcb76c3ea5baa5ac424a6f2
TanelPadar <Padar144@hotmail.com>
2022-09-22 02:05:36 +0300

    as user i can add users [Delivers #183344894]

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,63 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import React from "react";
+import Example from "./adduser";
+
+
 
 const Home = () => {
 
     const [posts, setPosts] = useState([]);
     const api = 'https://jsonplaceholder.typicode.com/users'
-    useEffect(()=>{
-        const getPosts = async () =>{
-            const {data:res} = await axios.get(api)
+    useEffect(() => {
+        const getPosts = async () => {
+            const {data: res} = await axios.get(api)
             setPosts(res)
         };
         getPosts();
     },[]);
+
+    const addPost = async () => {
+        const user = {name: 'username', username: 'New Username'};
+        await axios.post(api,user)
+        setPosts([user,...posts]);
+    };
+
+
+
     return (
         <>
-            <div className="container">
-                <h2>Hello world</h2>
-                <button className="btn btn-primary"> add post</button>
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th>name</th>
-                        <th>username</th>
-                        <th>email</th>
+
+        <div className="container">
+
+
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>name</th>
+                    <th>username</th>
+                    <th>email</th>
+                </tr>
+                </thead>
+                <tbody>
+                {posts.map(post =>
+                    <tr key={post.id}>
+                        <td>{post.name}</td>
+                        <td>{post.username}</td>
+                        <td>{post.email}</td>
+                        <td><button className="btn btn-info btn-sm"> update</button></td>
+                        <td><button className="btn btn-info btn-sm"> delete</button></td>
                     </tr>
-                    </thead>
-                    <tbody>
-                    {posts.map(post =>
-                        <tr key={post.id}>
-                            <td>{post.name}</td>
-                            <td>{post.username}</td>
-                            <td>{post.email}</td>
-                            <td><button className="btn btn-info btn-sm"> update</button></td>
-                            <td><button className="btn btn-info btn-sm"> delete</button></td>
-                        </tr>
-                    )}
-                    </tbody>
-                </table>
-            </div>
+                )}
+                </tbody>
+            </table>
+            <Example posts={posts} setPosts={setPosts}/>
+        </div>
         </>
     );
 };
+
+
+
 
 export default Home;
---
+++ src/components/adduser.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import axios from "axios";
+
+function Example(props) {
+    const [show, setShow] = useState(false);
+    const [state, setState] = React.useState({
+        id: 0,
+        name: "",
+        username: "",
+        email: ""
+    })
+    function handleChange(evt) {
+        const value = evt.target.value;
+        setState({
+            ...state,
+            [evt.target.name]: value
+        });
+    }
+
+    function saveUser() {
+        const api = 'https://jsonplaceholder.typicode.com/users'
+        state.id = props.posts.length+1;
+        axios.post(api,state).then(() => {
+            props.setPosts([state,...props.posts]);
+        })
+    }
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Add NEW USER
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add user</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" className="form-control" name="name" onChange={handleChange} value={state.name} id="name"
+                           placeholder="Example input"></input>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" className="form-control" name="username" onChange={handleChange} value={state.username} id="username"
+                           placeholder="Another input"></input>
+                    <label htmlFor="email">Email:</label>
+                    <input type="text" className="form-control" name="email" onChange={handleChange} value={state.email} id="email"
+                           placeholder="Another input"></input>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={saveUser}>
+                        Add
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+
+}
+export default Example;
