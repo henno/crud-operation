535799c88a4ce78cca7c29c84fcbad1702f757b6
TanelPadar <Padar144@hotmail.com>
2022-10-17 12:55:15 +0300

    added cypress tests.

---
+++ src/cypress/e2e/spec.cy.js
@@ -0,0 +1,24 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('html').click();
+    cy.get('tr:nth-child(10) .btn-danger').click();
+    cy.get('tr:nth-child(9) .btn-danger').click();
+    cy.get('tr:nth-child(8) .btn-primary').click();
+    cy.get('#name').click();
+    cy.get('#name').type('jaagup');
+    cy.get('#username').type('kreem');
+    cy.get('#email').type('kreem@hot.ee');
+    cy.get('.modal-footer > .btn-primary').click();
+    cy.get('.btn-close').click();
+    cy.get('.container > .btn').click();
+    cy.get('#name').click();
+    cy.get('#name').type('tiit');
+    cy.get('#username').type('marvel');
+    cy.get('#email').type('marvel@hot.ee');
+    cy.get('.modal-footer > .btn-primary').click();
+    cy.get('.btn-close').click();
+  })
+})
+
+
